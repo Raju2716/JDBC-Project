0359bb63c0cc165385577196f4643a1fa5defd3a
Raju2716 <130035806+Raju2716@users.noreply.github.com>
2023-05-04 13:41:41 +0530

    Add files via upload
    Adding files

---
+++ src/jdbc.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+import java.lang.Exception;
+
+class jdbc {
+    public static void main(String args[]) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/second", "root", "Raju@123");
+            // here sonoo is database name, root is username and password
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from  emp where empId=10");
+            while (rs.next())
+                System.out
+                        .println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + "  " + rs.getInt(4));
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
